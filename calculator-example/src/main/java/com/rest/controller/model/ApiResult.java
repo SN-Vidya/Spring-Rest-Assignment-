d9bae1155933b85566d4a2e2dfc784f2c58fbf07
SN-Vidya <snvidya1998@gmail.com>
2021-05-14 19:41:07 +0530

    calculator

---
+++ calculator-example/src/main/java/com/rest/controller/model/ApiResult.java
@@ -0,0 +1,31 @@
+package com.rest.controller.model;
+
+public class ApiResult {
+
+   private String result;
+   private String error;
+
+   private ApiResult() {
+   }
+
+   static ApiResult withResult(String resultMessage) {
+      ApiResult apiResult = new ApiResult();
+      apiResult.result = resultMessage;
+      return apiResult;
+   }
+
+   static ApiResult withError(String errorMessage) {
+      ApiResult apiResult = new ApiResult();
+      apiResult.error = errorMessage;
+      return apiResult;
+   }
+
+   public String getResult() {
+      return result;
+   }
+
+   public String getError() {
+      return error;
+   }
+}
+
